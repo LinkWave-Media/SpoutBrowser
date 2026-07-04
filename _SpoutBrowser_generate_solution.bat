64eb6e43359689233f38b63b2a618bf9b453e5a3
LinkWave-Media <felipe@linkwave.media>
2026-07-04 19:40:47 -0400

    Downgrade developer CEF SDK target to 149.0.4 to match NuGet runtime files, resolving Vimeo player mismatch

--- _SpoutBrowser_generate_solution.bat
+++ _SpoutBrowser_generate_solution.bat
@@ -18,7 +18,7 @@ setlocal
 :: Configuration
 
 :: See available distibutions: https://cef-builds.spotifycdn.com/index.html
-set CEF_DISTRIBUTION=cef_binary_149.0.6+g0d0eeb6+chromium-149.0.7827.201_windows64
+set CEF_DISTRIBUTION=cef_binary_149.0.4+g2f1bfd8+chromium-149.0.7827.156_windows64
 
 :: Spout tag (for CMake FetchContent_Declare)
 :: See avalable tags: https://github.com/leadedge/Spout2/tags
