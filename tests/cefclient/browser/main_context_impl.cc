458f36027e6458db70bff318e35cd6ccba5307d6
LinkWave-Media <felipe@linkwave.media>
2026-07-04 14:08:47 -0400

    Change default startup URL to Google

--- tests/cefclient/browser/main_context_impl.cc
+++ tests/cefclient/browser/main_context_impl.cc
@@ -18,8 +18,7 @@ namespace client {
 namespace {
 
 // The default URL to load in a browser window.
-//const char kDefaultUrl[] = "https://www.google.com";
-const char kDefaultUrl[] = "https://bntre.github.io/SpoutBrowser/"; // SpoutBrowser: custom splash screen
+const char kDefaultUrl[] = "https://www.google.com";
 
 // Returns the ARGB value for |color|.
 cef_color_t ParseColor(const std::string& color) {
